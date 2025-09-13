2dd00fbe0fba986766df26c69d803f9a60fd2abe
Mayank3299 <mayank.agarwal0903@gmail.com>
2025-09-14 03:58:52 +0530

    send custom delimeter in add function

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe 'String Calculator' do
     it 'return the sum of numbers with both newline and comma' do
       expect(StringCalculator.add('1\n2,3')).to eq(6)
     end
+
+    it 'return the sum with different delimeters support' do
+      expect(StringCalculator.add('//;\n1;2')).to eq(3)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,12 @@ class StringCalculator
     return 0 if input_str.empty?
     return input_str.to_i if input_str.length == 1
 
-    processed_str = input_str.tr('\n', ',')
-    processed_str.split(',').map(&:to_i).sum
+    delimeter = ','
+    if input_str.start_with?('//')
+      str_parts = input_str.split('\n')
+      delimeter = str_parts[0][2..]
+    end
+    processed_str = input_str.tr('\n', delimeter)
+    processed_str.split(delimeter).map(&:to_i).sum
   end
 end
