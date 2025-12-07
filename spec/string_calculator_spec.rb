124ed4be97f9c70e1fccba3e382b4665ec1c5095
Mayank3299 <mayank.agarwal0903@gmail.com>
2025-12-07 20:03:51 +0530

    EXTRA: Return product if delimeter is *

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,31 +3,35 @@ require './string_calculator'
 RSpec.describe 'String Calculator' do
   describe 'add' do
     it 'return 0 for an empty string' do
-      expect(StringCalculator.add('')).to eq(0)
+      expect(StringCalculator.calculate('')).to eq(0)
     end
 
     it 'return the number back if only 1 number is given' do
-      expect(StringCalculator.add('7')).to eq(7)
+      expect(StringCalculator.calculate('7')).to eq(7)
     end
 
     it 'returns the sum of the numbers in the input string' do
-      expect(StringCalculator.add('1,8')).to eq(9)
+      expect(StringCalculator.calculate('1,8')).to eq(9)
     end
 
     it 'return the sum of numbers with both newline and comma' do
-      expect(StringCalculator.add('1\n2,3')).to eq(6)
+      expect(StringCalculator.calculate('1\n2,3')).to eq(6)
     end
 
     it 'return the sum with different delimeters support' do
-      expect(StringCalculator.add('//;\n1;2')).to eq(3)
+      expect(StringCalculator.calculate('//;\n1;2')).to eq(3)
     end
 
     it 'throw an error when negative numbers are included' do
-      expect { StringCalculator.add('1,-2,3') }.to raise_error('negative numbers not allowed -2')
+      expect { StringCalculator.calculate('1,-2,3') }.to raise_error('negative numbers not allowed -2')
     end
 
     it 'return all passed negative numbers separated by comma' do
-      expect { StringCalculator.add('1,-2,3,-3,-8') }.to raise_error('negative numbers not allowed -2, -3, -8')
+      expect { StringCalculator.calculate('1,-2,3,-3,-8') }.to raise_error('negative numbers not allowed -2, -3, -8')
+    end
+
+    it 'return product of all the numbers when delimeter is *' do
+      expect(StringCalculator.calculate('//*\n1*2*3*4')).to eq(24)
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,22 +2,43 @@
 
 # Parses numbers from string, supports custom delimeter, will throw error for negative numbers
 class StringCalculator
-  def self.add(input_str)
+  def self.calculate(input_str)
     return 0 if input_str.empty?
     return input_str.to_i if input_str.length == 1
 
+    delimeter, input_str = StringCalculator.get_delimeter(input_str)
+
+    processed_str = input_str.tr('\n', delimeter)
+    digits = processed_str.split(delimeter).map(&:to_i)
+    StringCalculator.check_negatives(digits)
+
+    if delimeter == '*'
+      StringCalculator.product(digits)
+    else
+      StringCalculator.add(digits)
+    end
+  end
+
+  def self.check_negatives(digits)
+    negatives = digits.select(&:negative?)
+    raise "negative numbers not allowed #{negatives.join(', ')}" unless negatives.empty?
+  end
+
+  def self.add(digits)
+    digits.sum
+  end
+
+  def self.product(digits)
+    digits.inject { |product, num| product * num }
+  end
+
+  def self.get_delimeter(input_str)
     delimeter = ','
     if input_str.start_with?('//')
       str_parts = input_str.split('\n')
       delimeter = str_parts[0][2..]
+      input_str = str_parts[1]
     end
-
-    processed_str = input_str.tr('\n', delimeter)
-    digits = processed_str.split(delimeter).map(&:to_i)
-
-    negatives = digits.select(&:negative?)
-    raise "negative numbers not allowed #{negatives.join(', ')}" unless negatives.empty?
-
-    digits.sum
+    [delimeter, input_str]
   end
 end
